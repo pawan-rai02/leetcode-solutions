b44f3925fd70e368a5609077ea4b29d86b6eb25b
Pawan Kumar Rai <158175093+pawan-rai02@users.noreply.github.com>
2026-06-27 11:47:32 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-
-
-    int n;
-    int dp[10001];
-
-    bool solve(vector<int>& nums, int i) {
-
-        if (i >= n - 1)
-            return dp[i] = true;
-
-        if (nums[i] == 0)
-            return dp[i] = false;
-        
-        if(dp[i] != -1)
-            return dp[i];
-
-        int maxJump = nums[i];
-
-        for (int j = i + 1; j <= min(n - 1, i + maxJump); j++) {
-            if (solve(nums, j))
-                return dp[i] = true;
-        }
-
-        return dp[i] = false;
-
-    }
-
     bool canJump(vector<int>& nums) {
         
-        memset(dp, -1, sizeof(dp));
+        int n = nums.size();
 
-        n = nums.size();
+        int maxEnd = nums[0];
 
-        return solve(nums, 0);
+        if(maxEnd >= n - 1)
+            return true;
+
+        for(int i = 1; i < n; i++){
+
+            if(maxEnd < i)
+                return false;
+                
+            maxEnd = max(maxEnd, i + nums[i]);
+
+            if(maxEnd >= n - 1)
+                return true;
+        }
+
+        return false;
     }
 };
