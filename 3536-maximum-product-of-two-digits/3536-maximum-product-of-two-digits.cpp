3c1154b9b52ca69ba056ea5e9df9a39ff17bce3f
Pawan Kumar Rai <158175093+pawan-rai02@users.noreply.github.com>
2026-07-27 23:45:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.87%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int maxProduct(int n) {
+        int max1 = 0, max2 = 0;
+
+        while (n) {
+            int num = n % 10;
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2) {
+                max2 = num;
+            }
+            n /= 10;
+        }
         
-        vector<int> f(10, 0);
-
-        while(n > 0){
-            int ld = n % 10;
-            f[ld]++;
-
-            n = n / 10;
-        }
-
-        int fd = 0;
-        int sd = 0;
-
-        for(int i = 9; i >= 0; i--){
-            if(f[i] == 0)
-                continue;
-
-            if(f[i] > 0 && fd == 0){
-                fd = i;
-                f[i]--;
-            }
-
-            if(f[i] > 0 && sd == 0){
-                sd = i;
-                f[i]--;
-            }
-        }
-
-        return fd * sd;
+        return max1 * max2;
     }
 };
