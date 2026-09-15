92b61203348f0c543bc1e7b6c89023ac3824f609
Pawan Kumar Rai <158175093+pawan-rai02@users.noreply.github.com>
2026-09-15 15:28:28 +0530

    Time: 1698 ms (6.54%), Space: 208.1 MB (18.06%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
 
-    int n;
-    vector<vector<int>> dp;
-
     bool isPalin(string &s, int i, int j) {
 
         while (i < j) {
@@ -17,49 +14,33 @@ public:
         return true;
     }
 
-    int solve(string &s, int k, int i, int j) {
-
-        if (i >= n || j >= n)
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = 0;
-
-        if (isPalin(s, i, j)) {
-
-            // Take this palindrome
-            int takeIt = 1 + solve(s, k, j + 1, j + k);
-
-            // Extend current substring
-            int grow = solve(s, k, i, j + 1);
-
-            // Move both pointers
-            int slide = solve(s, k, i + 1, j + 1);
-
-            ans = max({takeIt, grow, slide});
-        }
-        else {
-
-            int grow = solve(s, k, i, j + 1);
-            int slide = solve(s, k, i + 1, j + 1);
-
-            ans = max(grow, slide);
-        }
-
-        return dp[i][j] = ans;
-    }
-
     int maxPalindromes(string& s, int k) {
-
-        n = s.size();
         
+        int n = s.size();
+
         if(k == 1)
             return n;
 
-        dp.assign(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
 
-        return solve(s, k, 0, k - 1);
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(isPalin(s, i, j)){
+
+                    int takeIt = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
+                    int grow   = dp[i][j + 1];
+                    int slide  = dp[i + 1][j + 1];
+
+                    dp[i][j] = max({takeIt, grow, slide});
+                }
+
+                int grow  = dp[i][j + 1];
+                int slide = dp[i + 1][j + 1];
+
+                dp[i][j] = max({dp[i][j], grow, slide});
+            }
+        }
+
+        return dp[0][k - 1];
     }
 };
